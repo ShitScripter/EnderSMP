40a2c66ed38f6f46475dd6f11ad31a88ffc71feb
bothost23 <64807538+bothost23@users.noreply.github.com>
2020-05-21 20:36:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,61 @@
-const Discord = require('discord.js');
+  const Discord = require('discord.js');
 const bot = new Discord.Client();
+const token = 'NzEyMDk4MjMzOTI4OTA4ODMw.XsNggQ.8pBfd3RlsYu_1Gr_SVpTHN60Xa8'
 
 bot.on('ready', () => {
     console.log('Bot is online');
 });
 
-bot.login(process.env.token);
+bot.on('message', msg=>{
+    if(msg.content === "!Ip"){
+        msg.reply('endersplace.crafted.pro -Version 1.14.4');
+    }
+})
+
+bot.on('message', msg=>{
+    if(msg.content === "pong"){
+        msg.reply('ping');
+    }
+})
+
+bot.on('message', msg=>{
+    if(msg.content === "kms"){
+        msg.reply('BANG');
+        msg.reply('BANG');
+    }
+})
+
+bot.on('message', msg=>{
+    if(msg.content === "are u here enderSMP"){
+        msg.reply('Yes I am here');
+    }
+})
+
+bot.on('message', msg=>{
+    if(msg.content === "can u ban someone for me"){
+        msg.reply('Yes, Who?');
+    }
+})
+
+
+bot.on('message', msg=>{
+    if(msg.content === "hi"){
+        msg.reply('Hello!!');
+    }
+})
+
+
+bot.on('message', msg=>{
+    if(msg.content === "ping"){
+        msg.reply('pong');
+    }
+})
+
+bot.on('message', msg=>{
+    if(msg.content === "!Rules"){
+        msg.reply('Respect and listen to staff, Respect others, Dont Curse, Have fun! See more info in #rules');
+    }
+})
+
+
+bot.login(token);
