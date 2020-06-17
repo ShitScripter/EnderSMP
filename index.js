0a3cab8a87e9eef5e4b88499966a0a6844fe920b
bothost23 <64807538+bothost23@users.noreply.github.com>
2020-06-17 15:02:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Variables
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const botsettings = require('./botsettings.json');
 //-------------------------
 
 bot.on('ready', () => {
@@ -140,11 +141,47 @@ bot.on("message", function(message) {
     
 });
 
+require("./util/eventHandler")(bot)
+
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+    if(err) console.log(err)
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js") 
+    if(jsfile.length <= 0) {
+         return console.log("[LOGS] Couldn't Find Commands!");
+    }
+
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/${f}`);
+        bot.commands.set(pull.config.name, pull);  
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        });
+    });
+});
+
+bot.on("message", async message => {
+    if(message.author.bot || message.channel.type === "dm") return;
+
+    let prefix = botsettings.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = message.content.substring(message.content.indexOf(' ')+1);
+
+    if(!message.content.startsWith(prefix)) return;
+    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
+    if(commandfile) commandfile.run(bot,message,args)
+
+})
+
+
 // Login / Run
 
 bot.login(process.env.token);
 
 //--------------
-
-
-console.log('update')
