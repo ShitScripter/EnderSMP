6feb8286c0d07f389a0f8022fb135e566e6ebcbd
bothost23 <64807538+bothost23@users.noreply.github.com>
2020-08-05 21:56:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -147,9 +147,10 @@ if (message.content === ">rules") {
 bot.on("message", function(messageF) {
     if (message.content === ">BulkDelete 10") {
         if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("You do not have permission");
+                message.delete()
                 message.channel.bulkDelete(10)
                 message.channel.send("Deleted <10> messages")
-                bot.message.delete()
+                message.delete()
         }
 });
 
