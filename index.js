9caab5ec05a063b283b59595074876a3fb4cc608
bothost23 <64807538+bothost23@users.noreply.github.com>
2020-08-05 21:23:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ bot.on("message", function(message) {
         if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("You do not have permission");
                 message.channel.bulkDelete(10)
                 message.channel.send("Deleted <10> messages")
-                message.content.delete()
+                message.delete()
         }
 });
 
